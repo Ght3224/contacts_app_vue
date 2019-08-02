324c4bf0eb767034255706b5d309bc8d57d59866
YOUR NAME <adien330@yahoo.com>
2019-08-01 22:30:24 -0400

    contacts vue app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+  <p>LastName: <input type="text" v-model="newContactName"><p>
+  <button v-on:click="createContact ()">create Contact</button>
+  <!-- </div> -->
+   <h1> {{message}}</h1>
+   <div v-for="contact in contacts">
+    <p> {{contact.first_name}} </p>
+  </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'; 
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "contacts",
+      contacts: [], 
+      newContactName: "",
+      newEmail: "", 
+      phoneNum: ""
+    };
+  },
+  created: function() {
+    console.log("i am contact");
+    axios.get('/api/contacts').then(response => {
+      this.contacts = response.data;
+    });
+  },
+  methods: {
+    createContact: function() {
+      console.log("testing");
+
+      var newContact = {
+        last_name: this.newContactName
+      }; 
+      axios.post('/api/contacts', newContact).then(response => {
+        console.log(response.data); 
+        this.contacts.push(response.data); 
+      }); 
+    }
   }
-}
+};
+
 </script>
